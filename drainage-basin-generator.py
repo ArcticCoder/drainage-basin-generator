9ccf76c138db465598a751877062f7caba120556
Vili Sinervä <vili.m.sinerva@gmail.com>
2022-02-24 18:16:20 +0200

    Add simple tkinter GUI

--- drainage-basin-generator.py
+++ drainage-basin-generator.py
@@ -1,10 +1,9 @@
-#import tkinter as tk
-
+import ctypes
 from PIL import Image
 import random
-import ctypes
+import tkinter as tk
 
-def data_from_image():
+def data_from_image(img, width, height, mode):
     inputGreyscale = [0]*width*height
 
     if(mode == "L" or mode == "P" or mode == "I"):
@@ -32,7 +31,7 @@ def data_from_image():
     return inputGreyscale
 
 
-def image_from_output(outputGreyscale):
+def image_from_output(img, outputGreyscale, width, height, mode):
     if(mode == "RGB"):
         RGB = [(0,0,0)]*width*height
         for i in range(width*height):
@@ -48,107 +47,99 @@ def image_from_output(outputGreyscale):
 
     img.putdata(outputGreyscale)
 
+def run_simulation():
+    try:
+        inputPath = i_filepath_ent.get()
+    except:
+        i_filepath_ent.delete(0,tk.END)
+        return
 
-def drainage_iteration():
-    pixelpos = 0
-    while True:
-        pixelpos = random.randrange(width*height)
-        if inputGreyscale[pixelpos] > 0:
-            break
+    try:
+        pathCount = int(path_count_ent.get())
+    except:
+        path_count_ent.delete(0,tk.END)
+        return
 
-    visited = set()
-    prevDx = 0
-    prevDy = 0
-    while True:
-        visited.add(pixelpos)
-        if mode == "I":
-            outputGreyscale[pixelpos] = min((outputGreyscale[pixelpos] + perPathI), pixelMaxI)
-        else:
-            outputGreyscale[pixelpos] = min((outputGreyscale[pixelpos] + perPath), pixelMax)
+    try:
+        outputPath = inputPath.split(".")
+        outputPath = f"{outputPath[0]}-{pathCount}.{outputPath[-1]}"
+    except:
+        i_filepath_ent.delete(0,tk.END)
+        return
 
-        if(inputGreyscale[pixelpos] == 0):
-            break
+    try:
+        perPath = int(per_path_ent.get())
+    except:
+        per_path_ent.delete(0,tk.END)
+        return
+    pixelMax = 255
+    pixelMaxI = (2**16)-1
+    perPathI = perPath * (pixelMaxI // pixelMax)
 
-        neighbours = []
-        for dx in range(-1,2):
-            for dy in range(-1,2):
-                neighbourPos = pixelpos+dy+dx*width
-                if neighbourPos not in visited:
-                    neighbours.append((inputGreyscale[neighbourPos], neighbourPos, dx, dy))
+    #LOADING
+    libcpp = ctypes.CDLL("./terraingen.so")
 
-        neighbours.sort()
+    try:
+        img = Image.open(inputPath)
+    except:
+        i_filepath_ent.delete(0,tk.END)
+        return
+    width, height = img.size
+    mode = img.mode
+    inputGreyscale = data_from_image(img, width, height, mode)
+    outputGreyscale = [0]*width*height
 
-        if len(neighbours) == 0:
-            break
-        minElev,_,_,_ = neighbours[0]
-        neighbours = [(e,p,dx,dy) for e,p,dx,dy in neighbours if e == minElev]
-        angles = []
-        for i in range(len(neighbours)):
-            value, pos, dx, dy = neighbours[i]
-            angle = abs(prevDx-dx) + abs(prevDy-dy)
-            angles.append((angle, pos, dx, dy))
+    c_inputGreyscale = (ctypes.c_int * len(inputGreyscale))(*inputGreyscale)
+    c_outputGreyscale = (ctypes.c_int * len(outputGreyscale))(*outputGreyscale)
 
-        angles.sort()
-        validNeighbour = False
-        for i in range(len(angles)):
-            angle, pos, dx, dy = angles[i]
-            if pos not in visited:
-                pixelpos = pos
-                prevDx = dx
-                prevDy = dy
-                validNeighbour = True
-                break
-        if not validNeighbour:
-            break
+    #ITERATIONS
+    #for i in range(pathCount):
+        #if i % 1000 == 0:
+        #    print(f"{i//1000}K")
+    #    drainage_iteration()
+    #CALL C++-lib for actual processing
+    if mode == "I":
+        libcpp.drainage_simulation(c_inputGreyscale, c_outputGreyscale, width, height, pathCount, perPathI, pixelMaxI)
+    else:
+        libcpp.drainage_simulation(c_inputGreyscale, c_outputGreyscale, width, height, pathCount, perPath, pixelMax)
+    outputGreyscale = list(c_outputGreyscale)
 
-#SETTINGS
-inputPath = "test.png"
-pathCount = int(1.0*10**5)
-perPath = 8
-pixelMax = 255
-pixelMaxI = (2**16)-1
-perPathI = perPath * (pixelMaxI // pixelMax)
-outputPath = f"test-{pathCount}.png"
-
-#LOADING
-libcpp = ctypes.CDLL("./terraingen.so")
-img = Image.open(inputPath)
-width, height = img.size
-mode = img.mode
-inputGreyscale = data_from_image()
-outputGreyscale = [0]*width*height
-
-c_inputGreyscale = (ctypes.c_int * len(inputGreyscale))(*inputGreyscale)
-c_outputGreyscale = (ctypes.c_int * len(outputGreyscale))(*outputGreyscale)
-
-#ITERATIONS
-#for i in range(pathCount):
-    #if i % 1000 == 0:
-    #    print(f"{i//1000}K")
-#    drainage_iteration()
-#CALL C++-lib for actual processing
-if mode == "I":
-    libcpp.drainage_simulation(c_inputGreyscale, c_outputGreyscale, width, height, pathCount, perPathI, pixelMaxI)
-else:
-    libcpp.drainage_simulation(c_inputGreyscale, c_outputGreyscale, width, height, pathCount, perPath, pixelMax)
-outputGreyscale = list(c_outputGreyscale)
-
-#OUTPUT
-image_from_output(outputGreyscale)
-img.save(outputPath)
-
+    #OUTPUT
+    image_from_output(img, outputGreyscale, width, height, mode)
+    img.save(outputPath)
 
 #Window definition
-#window = tk.Tk()
+window = tk.Tk()
 
 #Input filepath
-
-#Output filepath
+i_filepath_frm = tk.Frame()
+i_filepath_lbl = tk.Label(i_filepath_frm, text="Input image filepath:", anchor="w")
+i_filepath_ent = tk.Entry(i_filepath_frm)
+i_filepath_ent.insert(0,"input.png")
+i_filepath_frm.pack(fill=tk.X)
+i_filepath_lbl.pack(fill=tk.X)
+i_filepath_ent.pack(fill=tk.X)
 
 #Path count
+path_count_frm = tk.Frame()
+path_count_lbl = tk.Label(path_count_frm, text="Path count:", anchor="w")
+path_count_ent = tk.Entry(path_count_frm, width=30)
+path_count_ent.insert(0,"10000")
+path_count_frm.pack(fill=tk.X)
+path_count_lbl.pack(fill=tk.X)
+path_count_ent.pack(side=tk.LEFT)
 
-#RNG seed (opt)
+#Per path
+per_path_frm = tk.Frame()
+per_path_lbl = tk.Label(per_path_frm, text="Pixel value increase per path (0-255):", anchor="w")
+per_path_ent = tk.Entry(per_path_frm, width=30)
+per_path_ent.insert(0,"8")
+per_path_frm.pack(fill=tk.X)
+per_path_lbl.pack(fill=tk.X)
+per_path_ent.pack(side=tk.LEFT)
 
-#Live preview?
+#RUN-button
+run_btn = tk.Button(text="Run", command=run_simulation)
+run_btn.pack(fill=tk.BOTH)
 
-#window.mainloop()
+window.mainloop()
